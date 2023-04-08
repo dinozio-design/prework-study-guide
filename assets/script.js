ff55702852dc6701f3018a797421d1a88bd279fa
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-04-08 15:46:22 -0400

    bug?

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+// console.log(topics);
+console.log("dude this is awsome!");
+for (let i = 0; i < topics.length; i++){
+    console.log(topics[i]);
+}
+// first conditions. Must define input methode of selection -- adds above ^^
+/*
+var topic = "HTML"
+if (topic === 'HTML'){
+    console.log("let's code HTML!")
+} else if (topic === 'CSS'){
+    console.log("let's code CSS!")
+} else if(topic === 'Git'){
+    console.log("let's code Git!")
+} else if(topic === 'JavaScript'){
+    console.log("let's code JavaScript!")
+} else {console.log("Please Try Again!")}
+*/
