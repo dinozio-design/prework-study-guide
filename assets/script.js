510505fd7b15f47f1377083ea0bef561a5ca7cfa
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-04-08 17:55:11 -0400

    JavaScript Is Fun!

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
+// part 2
+function helloWorld() {
+    console.log("Hellow WOrld, I am a function!");
+}
+//
+// Declarations
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random()*topics.length)];
+//
+//functions
+
+function listTopics(){
+    for (let i = 0; i < topics.length; i++){
+        console.log(topics[i]);
+    }   
+}
+function selectTopic(){
+    if (randomTopic === 'HTML'){
+        console.log("let's code HTML!");
+    } else if (randomTopic === 'CSS'){
+        console.log("let's code CSS!");
+    } else if(randomTopic === 'Git'){
+        console.log("let's code Git!");
+    } else if(randomTopic === 'JavaScript'){
+        console.log("let's code JavaScript!");
+    } else {console.log("Please Try Again!");
+    }
+}
+//
+/*
+// part 1
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
 // console.log(topics);
 console.log("dude this is awsome!");
@@ -17,3 +48,7 @@ if (topic === 'HTML'){
     console.log("let's code JavaScript!")
 } else {console.log("Please Try Again!")}
 */
+console.log("here is a list of topics that we are learning in CodeCamp:");
+listTopics();
+console.log("which topic should we first start with?");
+selectTopic();
